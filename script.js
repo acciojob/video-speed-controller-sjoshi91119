ba5db3cbac1d2570f82a77f258ce2bbce6ef5f28
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-04-20 10:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-const inputs = document.querySelectorAll('.controls input');
+// Get all the elements we need
+const video = document.querySelector('.player__video');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const playBtn = document.querySelector('.player__button');
+const volumeRange = document.querySelector('[name="volume"]');
+const speedRange = document.querySelector('[name="playbackRate"]');
+const skipBtns = document.querySelectorAll('[data-skip]');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Function to toggle play/pause
+function togglePlay() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  playBtn.textContent = icon;
+  this.paused ? this.play() : this.pause();
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// Function to update progress bar
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+// Function to handle volume change
+function handleVolumeChange() {
+  video.volume = this.value;
+}
+
+// Function to handle playback speed change
+function handleSpeedChange() {
+  video.playbackRate = this.value;
+}
+
+// Function to handle skip buttons
+function handleSkip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+// Hook up the event listeners
+video.addEventListener('click', togglePlay);
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', handleProgress);
+volumeRange.addEventListener('change', handleVolumeChange);
+volumeRange.addEventListener('mousemove', handleVolumeChange);
+speedRange.addEventListener('change', handleSpeedChange);
+speedRange.addEventListener('mousemove', handleSpeedChange);
+skipBtns.forEach(button => button.addEventListener('click', handleSkip));
